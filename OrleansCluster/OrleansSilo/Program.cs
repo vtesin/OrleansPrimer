16146457a38366381299286727f294e42e4637a3
Vanja Tesin <v.tesin@gmail.com>
2017-02-28 23:35:16 +0100

    Added silo, client and interfaces

---
+++ OrleansCluster/OrleansSilo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Orleans.Runtime.Configuration;
+using OrleansSilo.OrleansSiloHost;
+
+namespace OrleansSilo
+{
+    class Program
+    {
+        private static OrleansHostWrapper _hostWrapper;
+
+        static int Main(string[] args)
+        {
+            int exitCode = StartSilo(args);
+
+            Console.WriteLine("Press Enter to terminate...");
+            Console.ReadLine();
+
+            exitCode += ShutdownSilo();
+
+            //either StartSilo or ShutdownSilo failed would result on a non-zero exit code. 
+            return exitCode;
+        }
+
+
+        private static int StartSilo(string[] args)
+        {
+            // define the cluster configuration
+            var config = ClusterConfiguration.LocalhostPrimarySilo();
+            config.AddMemoryStorageProvider();
+
+            _hostWrapper = new OrleansHostWrapper(config, args);
+            return _hostWrapper.Run();
+        }
+
+        private static int ShutdownSilo()
+        {
+            return _hostWrapper?.Stop() ?? 0;
+        }
+    }
+}
