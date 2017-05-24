8ff9c216f8d284d1d78f2697cb24c24868feb385
Vanja Tesin <v.tesin@gmail.com>
2017-05-25 00:48:41 +0200

    Enabled WebApi xml documenration provider.

--- AcmeSlotsApi/Acme.Slots.WebApi/Controllers/ValuesController.cs
+++ AcmeSlotsApi/Acme.Slots.WebApi/Controllers/ValuesController.cs
@@ -9,28 +9,50 @@ namespace Acme.Slots.WebApi.Controllers
 {
     public class ValuesController : ApiController
     {
+        /// <summary>
+        /// Get all the values that are currently stored in the system
+        /// </summary>
+        /// <returns>List of values</returns>
         // GET api/values
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
+        /// <summary>
+        /// Get the value by id
+        /// </summary>
+        /// <param name="id">Id of the value to retreive</param>
+        /// <returns>A value</returns>
         // GET api/values/5
         public string Get(int id)
         {
             return "value";
         }
 
+        /// <summary>
+        /// Post it!
+        /// </summary>
+        /// <param name="value">Value to post</param>
         // POST api/values
         public void Post([FromBody]string value)
         {
         }
 
+        /// <summary>
+        /// Put a value into the system
+        /// </summary>
+        /// <param name="id">An Id of the value</param>
+        /// <param name="value">Actual value</param>
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
+        /// <summary>
+        /// Deletes a value from the system
+        /// </summary>
+        /// <param name="id">An Id of the value</param>
         // DELETE api/values/5
         public void Delete(int id)
         {
